3a3ca23715f093b99c7c7ddafbca25d33c5bf992
Vinicios Trindade Costa <135274652+Vinhicious@users.noreply.github.com>
2024-10-10 11:01:37 -0300

    Update Teste.java

--- Teste.java
+++ Teste.java
@@ -9,6 +9,7 @@ class Teste {
         System.out.println("Prova UCB");
         System.out.println("Media: " + provaucb.calcularMedia());
         System.out.println("Situação: " + (provaucb.Aprovado()? "Aprovado" : "Não Aprovado"));
+        
         System.out.println("Prova Fafifo");
         System.out.println("Media: " + provafafifo.calcularMedia());
         System.out.println("Situação: " + (provafafifo.Aprovado()? "Aprovado" : "Não Aprovado"));
